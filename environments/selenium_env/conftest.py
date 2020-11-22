944f34a943bf5b2805ef9a73373abcd35906f326
anatolijtestqa <66serg12121255@gmail.com>
2020-11-22 14:34:58 +0300

    k

--- environments/selenium_env/conftest.py
+++ environments/selenium_env/conftest.py
@@ -1,9 +1,11 @@
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome")
     parser.addoption(
         "--language", action="store", default="en", help="my option: type1 or type2"
     )
@@ -11,10 +13,14 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='function')
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-    return (user_language, browser)
-    print("\nquit browser..")
+    browser = None
+    if browser_name == "chrome":
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome")
+    yield browser
     browser.quit()
